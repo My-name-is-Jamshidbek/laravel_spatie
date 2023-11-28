67c37d620d5b60ef6bca915671a6a498e9734fe4
Jamshidbek <jamshidbekollanazarov@gmail.com>
2023-11-29 00:51:09 +0500

    seeders setted up

--- config/database/seeders/AdminSeeder.php
+++ config/database/seeders/AdminSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class AdminSeeder extends Seeder
 {
     /**
@@ -12,6 +15,10 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+        ])->assignRole('writer', 'admin');
     }
 }
